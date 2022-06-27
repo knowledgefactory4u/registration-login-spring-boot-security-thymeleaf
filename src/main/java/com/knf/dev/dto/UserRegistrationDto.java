c3134e89d25bde5dfce9b30b77328b3a6502a07e
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2022-06-27 22:45:07 +0530

    Add files via upload
    init

---
+++ src/main/java/com/knf/dev/dto/UserRegistrationDto.java
@@ -0,0 +1,45 @@
+package com.knf.dev.dto;
+
+public class UserRegistrationDto {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+
+	public UserRegistrationDto() {
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
