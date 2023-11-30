aa05e01b0e56a6867dbae2e2a25b83c5b6564751
Sibin Muhammed <54763130+knowledgefactory4u@users.noreply.github.com>
2023-11-30 17:39:44 +0530

    Update UserRepository.java

--- src/main/java/com/knf/dev/repository/UserRepository.java
+++ src/main/java/com/knf/dev/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.knf.dev.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.knf.dev.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
